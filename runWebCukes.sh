958636379d469c84bebdb1bc76fe280aca093b95
Markus Silpala <msilpala@users.noreply.github.com>
2019-04-23 15:04:02 -0500

    Add shebang for my mac.

--- runWebCukes.sh
+++ runWebCukes.sh
@@ -1,2 +1,3 @@
+#!/usr/bin/env bash
 echo EquineHoroscope webserver and moco Merchant Bank servers should already be running
 mvn -Dtest=RunCukesTest -Dtags=@Web -DEquineHoroscope.testDepth=web -Dwebdriver.gecko.driver=lib/drivers/geckodriver -Dwebdriver.chrome.driver=lib/drivers/chromedriver -Dbrowser.name=chrome test
